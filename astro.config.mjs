e9e89cd013dfb4505072e565d29277585b1a5b96
iruma-tea <shusuke.think.pad@gmail.com>
2024-08-21 08:56:44 +0900

    単一のルートパラメータの学習完了

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+  // prefetch: {
+  //   prefetchAll: true,
+  //   defaultStrategy: "viewport",
+  // },
   integrations: [tailwind(), react()],
 });
